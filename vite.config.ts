ac60a3786b717c0b2a886dc7a9a5466589f32f85
leehoemun <leehoemun@gmail.com>
2025-09-19 14:52:10 +0800

    fix: include buffer polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),   tailwindcss()],
+  plugins: [react(), tailwindcss(), nodePolyfills()],
   define: {
-    global: "globalThis"
-  }
-})
+    global: "globalThis",
+  },
+});
